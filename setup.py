e451209a3894887f7483263b334b129ca5759608
= <=>
2020-09-11 10:14:31 +0700

    xd

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+requirements = [l.strip() for l in open('requirements.txt').readlines()]
+
+setup(
+     name='django_evercookie',
+     version='0.1',
+     install_requires = requirements,
+     packages = find_packages(),
+     include_package_data = True,
+     author = 'Phong Dep Zai',
+     author_email = 'phongkhonghoang@gmail.com',
+     description = ' Django',
+     license = 'MIT License',
+     keywords = 'django evercookie',
+     url='https://github.com/phonghoangkhong/test213',
+     )
